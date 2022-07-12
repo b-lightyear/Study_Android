d294b5be5ad87af64edc48b37e3be3b9a5397fc3
b-lightyear <tlsgksthf15@gmail.com>
2022-07-12 18:01:28 +0900

    22.07.12 수업

---
+++ And01_HelloWorld/app/src/main/java/com/example/and01_helloworld/StudentMain.java
@@ -0,0 +1,19 @@
+package com.example.and01_helloworld;
+
+public class StudentMain {
+
+    //주석 달기
+
+    //변수 선언
+    int num1, num2;
+
+    //메인 메소드 만들기
+    public static void main(String[] args) {
+        StudentMain student = new StudentMain();
+        student.num1 = 10;
+
+        System.out.println(student.num1);
+    }
+
+
+}//class
