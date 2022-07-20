cbccab487b21aae8c3d71f9afd57bf9604073ef3
b-lightyear <tlsgksthf15@gmail.com>
2022-07-20 09:28:36 +0900

    22.07.19 수업

---
+++ And06_ScrollView/app/src/main/java/com/example/and06_scrollview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.and06_scrollview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*//setText로 나의 이니셜로 텍스트뷰 글씨 바꿔보기
+        TextView textView = findViewById(R.id.tv_text);
+        textView.setText("SHS");
+*/
+       /* //글씨 붙이는 메소드
+        //append : 기존 글씨에 현재 붙일 String을 더한다.
+        for (int i = 0; i < 100; i++) {
+            textView.append("ㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇ");
+        }*/
+
+
+
+    }
+}
---
+++ And08_Resource/app/src/main/java/com/example/and08_resource/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.and08_resource;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawble_test);
+
+        Log.d("리소스값", " " + R.string.string_shs);
+        Log.d("리소스값", " " + getString(R.string.string_2));
+
+    }
+}
